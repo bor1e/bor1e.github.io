1ebaf21c32bb70f1e92ce8f52075f416007c962b
bor1e <ilija.borodin@fau.de>
2019-05-27 11:41:00 +0200

    update site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Code Snippets",
+        "excerpt":"Whenever you need to post a code snippet, use the liquid tags hilight and endhilight like this:   # some code goes here puts \"Hello World!\"  Note that this only provides color-coding. For that you might need to use a front end colorization engine like Highlight.JS or something similar.  ","categories": ["Design"],
+        "tags": [],
+        "url": "http://localhost:4000/design/code-snippets/",
+        "teaser":"http://localhost:4000/assets/images/teaser.png"}]
